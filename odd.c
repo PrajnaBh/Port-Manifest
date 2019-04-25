83955ac6db6d501a5ad2ed7f39219f405798bb34
PrajnaBh <49976513+PrajnaBh@users.noreply.github.com>
2019-04-25 15:55:22 +0530

    Add files via upload
    final

--- odd.c
+++ odd.c
@@ -4,19 +4,18 @@ void main()
 {
 char ch;
 int n;
-printf("Enter your Number:");
-scanf("%d",&n);
+
 do
 {
-    printf("\nDo you want to enter one more number(y/n):");
-scanf("%c",&ch);
+
 printf("Enter your Number:");
 scanf("%d",&n);
 if(n%2!=0)
 printf("Entered number is odd");
 else
 printf("Entered number is even");
-
+printf("\nDo you want to enter one more number(y/n):");
+scanf("%s",&ch);
 
 }
 while(ch=='y');
